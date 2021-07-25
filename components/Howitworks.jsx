44207f11b7fe6adf861034250c12ea4335347766
devansh dixit <devanshdixit01@gmail.com>
2021-07-25 22:20:12 +0530

    updated text

--- components/Howitworks.jsx
+++ components/Howitworks.jsx
@@ -3,69 +3,71 @@ import styles from "../styles/Howitworks.module.css"
 
 const HowItWorks = () => {
     return (
-        <div  style={{ backgroundColor: "#FCFCFC" }}>
-        <div className="container mt-4 pt-4 mb-4 pb-4">
-            <h1 className={styles.header}>Why Quecup?</h1>
-            <hr className={styles.hr} />
-            <div className="row mt-4 pt-4">
-                <div className={styles.trio}>
-                    <div className="col-md-4">
-                        <div className={styles.card}>
-                            <div className={styles.subheader}>Free and open for all</div>
-                            <div className={styles.cardheader}>levelZero</div>
-                            <div className="text">
-                                <ul className="list">
-                                    <li className="list-item">Start your developer journey here.</li>
-                                    <li className="list-item">Make 8 mini-apps and host them.</li>
-                                    <li className="list-item">Learn basic HTML, CSS, vanillaJS, Git, Github, React basics as well.</li>
-                                </ul>
+        <div style={{ backgroundColor: "#FCFCFC" }}>
+            <div className="container mt-4 pt-4 mb-4 pb-4">
+                <h1 className={styles.header}>Why Quecup?</h1>
+                <hr className={styles.hr} />
+                <div className="row mt-4 pt-4">
+                    <div className={styles.trio}>
+                        <div className="col-md-4">
+                            <div className={styles.card}>
+                                <div className={styles.subheader}>Free and open for all</div>
+                                <div className={styles.cardheader}>levelZero</div>
+                                <div className="text">
+                                    <ul className="list">
+                                        <li className="list-item">Start to know about campaigns information</li>
+                                        <li className="list-item">How they are spending the money </li>
+                                        <li className="list-item">Whether they can be trsuted or not</li>
+                                    </ul>
+                                </div>
+                                <div className="card-arrow">
+                                    <span className="text">Learn More</span>
+                                    <img src="/images/arrow.svg" alt="arrow" />
+                                </div>
                             </div>
-                            <div className="card-arrow">
+                        </div>
+                        <div className="col-md-4">
+                            <div className={styles.card}>
+                                <div className={styles.subheader}>focused group</div>
+                                <div className={styles.cardheader}>levelOne</div>
+                                <div className="text">
+                                    <ul className="list">
+                                        <li className="list-item">Background check on the company</li>
+                                        <li className="list-item">Verify Documents</li>
+                                        <li className="list-item">Start to know about campaigns information</li>
+                                        <li className="list-item">How they are spending the money </li>
+                                        <li className="list-item">Whether they can be trsuted or not</li>
+                                    </ul>
+                                </div>
+                                <div className="card-arrow"></div>
                                 <span className="text">Learn More</span>
-                                <img src="/images/arrow.svg" alt="arrow"/>
+                                <img src="/images/arrow.svg" alt="arrow" />
+                            </div>
+                        </div>
+                        <div className="col-md-4">
+                            <div className={styles.card}>
+                                <div className={styles.subheader}>Donation Assistance</div>
+                                <div className={styles.cardheader}>Let us handle your Donations</div>
+                                <div className="text">
+                                    <ul className="list">
+                                        <li className="list-item">Background check on the company</li>
+                                        <li className="list-item">Personal Assistant while donating</li>
+                                        <li className="list-item">Doing physical checks on campaigns</li>
+                                        <li className="list-item">Verify Documents</li>
+                                        <li className="list-item">Start to know about campaigns information</li>
+                                        <li className="list-item">How they are spending the money </li>
+                                        <li className="list-item">Whether they can be trsuted or not</li>
+                                    </ul>
+                                </div>
+                                <div className="card-arrow">
+                                    <span className="text">Learn More</span>
+                                    <img src="/images/arrow.svg" alt="arrow" />
+                                </div>
                             </div>
                         </div>
                     </div>
-                    <div className="col-md-4">
-                        <div className={styles.card}>
-                            <div className={styles.subheader}>Closed focused group</div>
-                            <div className={styles.cardheader}>levelOne</div>
-                            <div className="text">
-                                <ul className="list">
-                                    <li className="list-item">Live classes on Zoom.</li>
-                                    <li className="list-item">An intermediate level, 6 months of structured learning.</li>
-                                    <li className="list-item">Learn Advanced React, React Ecosystem, and more.</li>
-                                    <li className="list-item">Create 10 apps to showcase your learning.</li>
-                                    <li className="list-item">Learn to make your app full stack with ExpressJS + Database, Auth.</li>
-                                    <li className="list-item">Get interview-ready with advanced JS, CSS, and Typescript.</li>
-                                </ul>
-                            </div>
-                            <div className="card-arrow"></div>
-                            <span className="text">Learn More</span>
-                            <img src="/images/arrow.svg" alt="arrow"/>
-                        </div>
-                    </div>
-                <div className="col-md-4">
-                    <div className={styles.card}>
-                    <div className={styles.subheader}>Job placement Assistance</div>
-                        <div className={styles.cardheader}>Get Placed</div>
-                        <div className="text">
-                            <ul className="list">
-                                <li className="list-item">You complete levelOne with advanced-level projects.</li>
-                                <li className="list-item">You can directly apply to our hiring partner, roc8.careers with these projects.</li>
-                                <li className="list-item">roc8.careers will groom you further for interviews.</li>
-                                <li className="list-item">Interview through hiring partners and get a job!</li>
-                            </ul>
-                        </div>
-                        <div className="card-arrow">
-                            <span className="text">Learn More</span>
-                            <img src="/images/arrow.svg" alt="arrow"/>
-                        </div>
-                    </div>
-                </div>
                 </div>
             </div>
-        </div>
         </div>
     )
 }
--- components/Whyus.jsx
+++ components/Whyus.jsx
@@ -11,7 +11,7 @@ const Whyus = () => {
           <div><img src="/images/whyus/beginner_friendly.svg" alt="" /></div>
           <div className={styles.subheading}>Info about campaigners</div>
           <p className={styles.para}>
-            You have <strong>never written a donated a single rupee</strong>. No problem, we got you covered.
+            You have <strong>never donated a single rupee</strong>. No problem, we got you covered.
             Start with<strong>Quescup</strong>. Quescup is for donors who are in doubt of campaigns.
           </p>
         </div>
@@ -19,16 +19,16 @@ const Whyus = () => {
           <div><img src="/images/whyus/screen.svg" alt="" /></div>
           <div className={styles.subheading}>Verify Documents</div>
           <p className={styles.para}>
-            You have <strong>never written a donated a single rupee</strong>. No problem, we got you covered.
-            Start with<strong>Quescup</strong>. Quescup is for donors who are in doubt of campaigns.
+            Had any doubts about campaigns, tell us about that and 
+            we would dig up their documents and tell you whether they are legit or not 
           </p>
         </div>
         <div className="col-md-4 mt-4">
           <div><img src="/images/whyus/hill.svg" alt="" /></div>
           <div className={styles.subheading}>How the donation is used</div>
           <p className={styles.para}>
-            You have <strong>never written a donated a single rupee</strong>. No problem, we got you covered.
-            Start with<strong>Quescup</strong>. Quescup is for donors who are in doubt of campaigns.
+            We will go to full extent to know how your hard earned money
+            is spent and whether it goes to where it is promised or in someones bnakaccount.
           </p>
         </div>
       </div>
@@ -37,24 +37,24 @@ const Whyus = () => {
           <div><img src="/images/whyus/people.svg" alt="" /></div>
           <div className={styles.subheading}>Dedicated Campaign Checkers</div>
           <p className={styles.para}>
-            You have <strong>never written a donated a single rupee</strong>. No problem, we got you covered.
-            Start with<strong>Quescup</strong>. Quescup is for donors who are in doubt of campaigns.
+            We are here and we are here with a team that digs deep on that campaign
+            and show what is being hidden from you.
           </p>
         </div>
         <div className="col-md-4 mt-4">
           <div><img src="/images/whyus/youtube.svg" alt="" /></div>
           <div className={styles.subheading}>A-Z about Campaigners</div>
           <p className={styles.para}>
-            You have <strong>never written a donated a single rupee</strong>. No problem, we got you covered.
-            Start with<strong>Quescup</strong>. Quescup is for donors who are in doubt of campaigns.
+            From start to end, we will let you know what it is all about, when they started,
+            when 
           </p>
         </div>
         <div className="col-md-4 mt-4">
           <div><img src="/images/whyus/interview.svg" alt="" /></div>
           <div className={styles.subheading}>Promote Trusted Campaigns</div>
           <p className={styles.para}>
-            You have <strong>never written a donated a single rupee</strong>. No problem, we got you covered.
-            Start with<strong>Quescup</strong>. Quescup is for donors who are in doubt of campaigns.
+            We are the people who respect your trust and we are going to de anything 
+            to let you trust us no matter what.
           </p>
         </div>
       </div>
