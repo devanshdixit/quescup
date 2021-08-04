6a06e05cddf821d52a3865216adb5a34b034a333
devansh dixit <devanshdixit01@gmail.com>
2021-08-04 11:30:33 +0530

    nav updated

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -5,26 +5,26 @@ const Footer = () => {
     return (
         <footer className={styles.footercontainermain} style={{ backgroundColor: "#0D3344" }}>
             <div className="mt-4 pt-4 text-white">
-            <div className="container mt-4 ">
-                <div className="text-center">
-                    <h1>Join our community</h1>
+                <div className="container mt-4 ">
+                    <div className="text-center">
+                        <h1>Join our community</h1>
+                    </div>
+                    <div className="d-flex flex-wrap justify-content-center mt-3 mb-3">
+                        <div className="p-2"><a href="https://www.facebook.com/quescup.india"><img src="/images/social/instagram.svg" style={{ width: "2em", marginRight: ".5rem" }} alt="" /></a></div>
+                        <div className="p-2"><a href="https://www.instagram.com/p/CO7I-3IBMio/?utm_medium=copy_link"><img src="/images/social/linkedin.svg" style={{ width: "2em", marginRight: ".5rem" }} alt="" /></a></div>
+                        <div className="p-2"><img src="/images/social/telegram.svg" style={{ width: "2em", marginRight: ".5rem" }} alt="" /></div>
+                        <div className="p-2"><a href="https://twitter.com/quescup?s=08"><img src="/images/social/twitter.svg" style={{ width: "2em", marginRight: ".5rem" }} alt="" /></a></div>
+                        <div className="p-2"><img src="/images/social/youtube.svg" style={{ width: "2em", marginRight: ".5rem" }} alt="" /></div>
+                    </div>
+                    <div className="d-flex flex-wrap justify-content-center pb-4 pt-4 mt-3 mb-3">
+                        <div className="p-2 ml-3">Terms & Condition</div>
+                        <div className="p-2 " style={{ marginLeft: "40px", marginRight: "40px" }}>Privacy Policy</div>
+                        <div className="p-2 mr-3">Community Guidelines</div>
+                    </div>
                 </div>
-                <div className="d-flex flex-wrap justify-content-center mt-3 mb-3">
-                    <div className="p-2"><a href="https://www.facebook.com/quescup.india"><img src="/images/social/instagram.svg" style={{width:"2em",marginRight:".5rem"}} alt="" /></a></div>
-                    <div className="p-2"><a href="https://www.instagram.com/p/CO7I-3IBMio/?utm_medium=copy_link"><img src="/images/social/linkedin.svg" style={{width:"2em",marginRight:".5rem"}} alt="" /></a></div>
-                    <div className="p-2"><img src="/images/social/telegram.svg" style={{width:"2em",marginRight:".5rem"}} alt="" /></div>
-                    <div className="p-2"><a href="https://twitter.com/quescup?s=08"><img src="/images/social/twitter.svg" style={{width:"2em",marginRight:".5rem"}} alt="" /></a></div>
-                    <div className="p-2"><img src="/images/social/youtube.svg" style={{width:"2em",marginRight:".5rem"}} alt="" /></div>
+                <div className="container-fluid " style={{ backgroundColor: "#052634" }}>
+                    <div className="text-center mt-4 pt-4 pb-4">@Quescup. All rights Reserved</div>
                 </div>
-                <div className="d-flex flex-wrap justify-content-center pb-4 pt-4 mt-3 mb-3">
-                    <div className="p-2 ml-3">Terms & Condition</div>
-                    <div className="p-2 " style={{marginLeft:"40px",marginRight:"40px"}}>Privacy Policy</div>
-                    <div className="p-2 mr-3">Community Guidelines</div>
-                </div>
-            </div>
-            <div className="container-fluid " style={{backgroundColor:"#052634"}}>
-                <div className="text-center mt-4 pt-4 pb-4">@Quescup. All rights Reserved</div>
-            </div>
             </div>
         </footer>
     )
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,27 +1,42 @@
 /* eslint-disable @next/next/no-img-element */
-import { Col, Container, Nav, Navbar, Row } from "react-bootstrap"
-import styles from  '../styles/Header.module.css'
+import { Col, Container, Nav, Navbar, NavDropdown, Row } from "react-bootstrap"
+import styles from '../styles/Header.module.css'
 
 const Header = () => {
     function scrollto() {
         const contact = window.document.getElementById("contact");
         contact.scrollIntoView();
-      }
+    }
     return (
-        <Container className="p-2 mt-2 pl-1 pr-1">
-            <div className="row">
-                <div className="col"><img src={`/images/logo.PNG`}  width="100" height="100" alt="" /></div>
-                <div className="col pt-4" style={{maxWidth:"120px"}}>
-                    <div className={styles.headerlink}style={{paddingLeft:".8em"}}>About</div>
-                </div>
-                <div className="col pt-4" style={{maxWidth:"120px"}}>
-                    <div className={styles.headerlink}>Services</div>
-                </div>
-                <div className="col pt-4" style={{maxWidth:"120px"}}>
-                    <div className={styles.headerlink} onClick={scrollto}>ContactUs</div>
-                </div>
-            </div>
-        </Container>
+        <Navbar collapseOnSelect expand="lg" bg="white" variant="light">
+            <Container>
+                <Navbar.Brand href="#home"><img src={`/images/logo.PNG`} className={styles.imgsize} alt="" /></Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="me-auto">
+                    </Nav>
+                    <Nav className="ml-auto">
+                        <Nav.Link href="#deets" className={styles.headerlink} style={{ paddingLeft: ".8em" }}>About</Nav.Link>
+                        <Nav.Link href="#deets" className={styles.headerlink} style={{ paddingLeft: ".8em" }}>Services</Nav.Link>
+                        <Nav.Link href="#deets" className={styles.headerlink} style={{ paddingLeft: ".8em" }}>ContactUs</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+        // <Container className="p-2 mt-2 pl-1 pr-1">
+        //     <div className="row">
+        //         <div className="col bg-danger" style={{ maxWidth: "70px" }}><img src={`/images/logo.PNG`} className={styles.imgsize} alt="" /></div>
+        //         <div className="col bg-success pt-4" style={{ maxWidth: "120px" }}>
+        //             <div className={styles.headerlink} style={{ paddingLeft: ".8em" }}>About</div>
+        //         </div>
+        //         <div className="col bg-warning pt-4" style={{ maxWidth: "120px" }}>
+        //             <div className={styles.headerlink}>Services</div>
+        //         </div>
+        //         <div className="col bg-danger pt-4" style={{ maxWidth: "120px" }}>
+        //             <div className={styles.headerlink} onClick={scrollto}>ContactUs</div>
+        //         </div>
+        //     </div>
+        // </Container>
     )
 }
 export default Header
