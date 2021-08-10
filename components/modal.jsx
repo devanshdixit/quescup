eaab8b3a4c89dfced310994ccbb5cd276a06969a
devansh dixit <devanshdixit01@gmail.com>
2021-08-11 03:20:31 +0530

    done

--- components/modal.jsx
+++ components/modal.jsx
@@ -1,41 +1,104 @@
 import { useRouter } from 'next/dist/client/router';
 import { useState } from 'react';
-import { Button, Container, Form, Modal } from 'react-bootstrap';
+import { Button, Form, Modal } from 'react-bootstrap';
 import styles from '../styles/Modal.module.css'
-
+import firebase from "firebase/app";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 export default function ModalScreen() {
     const [show, setShow] = useState(false);
+    const [loading, setLoading] = useState(false);
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
     const [validated, setValidated] = useState(false);
     const [name, setName] = useState('');
-    const [address, setAddress] = useState('');
-    const [address1, setAddress1] = useState('');
-    const [city, setCity] = useState('');
-    const [state, setState] = useState('');
-    const [zip, setZip] = useState('');
+    const [image, setImage] = useState('');
+    const [email, setEmail] = useState('');
+    const [contactNo, setContactNo] = useState('');
+    const [campLink, setCampLink] = useState('');
     const router = useRouter();
-
-    const handleSubmit = (event) => {
+    const handleSubmit = async (event) => {
+        setLoading(true);
         const form = event.currentTarget;
         if (form.checkValidity() === false) {
             event.preventDefault();
             event.stopPropagation();
+            setLoading(false);
         }
         if (form.checkValidity() === true) {
             event.preventDefault();
-            addAddress({
-                'name': name,
-                'addess': address,
-                'address1': address1,
-                'city': city,
-                'state': state,
-                'zip': zip,
-            });
-            router.push("/payment");
+            uploadImage(image);
         }
         setValidated(true);
     };
+    const handleFileUpload = async (e) => {
+        if (e.target.files[0]) {
+            setImage(e.target.files[0]);
+        }
+    };
+
+    const uploadImage = () => {
+        var storageRef = firebase.storage().ref();
+        var db = firebase.firestore();
+        var metadata = {
+            contentType: 'image/jpeg'
+        };
+        var uploadTask = storageRef.child('images/' + campLink).put(image, metadata);
+        uploadTask.on(firebase.storage.TaskEvent.STATE_CHANGED,
+            (snapshot) => {
+                var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                console.log('Upload is ' + progress + '% done');
+                switch (snapshot.state) {
+                    case firebase.storage.TaskState.PAUSED: // or 'paused'
+                        console.log('Upload is paused');
+                        break;
+                    case firebase.storage.TaskState.RUNNING: // or 'running'
+                        console.log('Upload is running');
+                        break;
+                }
+            },
+            (error) => {
+                switch (error.code) {
+                    case 'storage/unauthorized':
+                        break;
+                    case 'storage/canceled':
+                        break;
+                    case 'storage/unknown':
+                        break;
+                }
+            },
+            () => {
+                uploadTask.snapshot.ref.getDownloadURL().then((downloadURL) => {
+                    try {
+                        var data = {
+                            'name': name,
+                            'email': email,
+                            'contactNo': contactNo,
+                            'CampLink': campLink,
+                            'imageLink': downloadURL,
+                        };
+                        db.collection("queries").add(data).then((docRef) => {
+                            handleClose();
+                            setLoading(false);
+                            router.push("/");
+                            toast.success('Your campaign is under review we contact you once we get anything!', {
+                                position: "bottom-right",
+                                autoClose: 5000,
+                                hideProgressBar: false,
+                                closeOnClick: true,
+                                pauseOnHover: true,
+                                draggable: true,
+                                progress: undefined,
+                            });
+                        })
+                            .catch((error) => {
+                            });
+                    } catch (e) {
+                    }
+                });
+            }
+        );
+    }
 
     return (
         <section id="contact" className="container" style={{ paddingBottom: "100px", paddingTop: "100px" }}>
@@ -80,10 +143,10 @@ export default function ModalScreen() {
                                 type="text"
                                 placeholder="Enter EmailId"
                                 required
-                                defaultValue={address}
+                                defaultValue={email}
                                 onChange={(e) => {
                                     e.preventDefault();
-                                    setAddress(e.target.value)
+                                    setEmail(e.target.value)
                                 }}
                             />
                             <Form.Control.Feedback type="invalid">
@@ -95,10 +158,10 @@ export default function ModalScreen() {
                             <Form.Control
                                 type="text"
                                 placeholder="Enter contact no"
-                                defaultValue={address1}
+                                defaultValue={contactNo}
                                 onChange={(e) => {
                                     e.preventDefault();
-                                    setAddress1(e.target.value)
+                                    setContactNo(e.target.value)
                                 }}
                             />
                         </Form.Group>
@@ -108,19 +171,19 @@ export default function ModalScreen() {
                                 type="text"
                                 placeholder="Enter Link of campaign"
                                 required
-                                defaultValue={city}
+                                defaultValue={campLink}
                                 onChange={(e) => {
                                     e.preventDefault();
-                                    setCity(e.target.value)
+                                    setCampLink(e.target.value)
                                 }}
                             />
                             <Form.Control.Feedback type="invalid">
-                                Please provide a valid city.
+                                Please provide a valid link.
                             </Form.Control.Feedback>
                         </Form.Group>
                         <Form.Group controlId="formFile" className="mt-3 mb-3">
-                            <Form.Label>Select images or pdf to upload :</Form.Label>
-                            <Form.Control type="file" />
+                            <Form.Label >Select images or pdf to upload :</Form.Label>
+                            <Form.Control onChange={(e) => handleFileUpload(e)} type="file" />
                         </Form.Group>
                         <Form.Group className="mt-3 mb-3">
                             <Form.Check
@@ -129,16 +192,26 @@ export default function ModalScreen() {
                                 feedback="You must agree before submitting."
                             />
                         </Form.Group>
-
                     </Form>
                 </Modal.Body>
                 <Modal.Footer>
                     <Button variant="secondary" onClick={handleClose}>
                         Close
                     </Button>
-                    <button className="btn btn-dark btn-block" type="submit">Submit</button>
+                    <button className="btn btn-dark btn-block" onClick={handleSubmit} type="submit">{loading ? 'Uploading...' : "Submit"}</button>
                 </Modal.Footer>
             </Modal>
+            <ToastContainer
+                position="bottom-right"
+                autoClose={5000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+            />
         </section>
     );
 }
--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,28 @@
-import '../styles/bootstrap-5.0.2-dist/css/bootstrap.css'
-import '../styles/bootstrap-5.0.2-dist/css/bootstrap.min.css'
-import '../styles/globals.css'
+import "../styles/bootstrap-5.0.2-dist/css/bootstrap.css";
+import "../styles/bootstrap-5.0.2-dist/css/bootstrap.min.css";
+import "../styles/globals.css";
+import firebase from "firebase/app";
+import "firebase/analytics";
+import "firebase/firestore";
+import "firebase/storage";
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const firebaseConfig = {
+    apiKey: "AIzaSyBPQzaXF585mXdmlDkTRt-eLsLWgWP4WUw",
+    authDomain: "final-feef1.firebaseapp.com",
+    databaseURL: "https://final-feef1.firebaseio.com",
+    projectId: "final-feef1",
+    storageBucket: "final-feef1.appspot.com",
+    messagingSenderId: "927190720438",
+    appId: "1:927190720438:web:6ef1f3a1d56a046c50eb17",
+    measurementId: "G-2Q9JNTQCFL",
+  };
+  if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+  } else {
+    firebase.app(); // if already initialized, use that one
+  }
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
